c323dfd478e0f5c5ff1b4f20f757ad9606e168e6
kokihoon <rhrlgns777@naver.com>
2017-02-28 11:20:25 +0900

    Sort

---
+++ SelectionSort.c
@@ -0,0 +1,126 @@
+#include <stdio.h>
+void printArray(int values[], int count);
+void selectionSort(int values[], int count);
+void BubbleSort(int values[], int count);
+void BubbleSort(int values[], int count);
+void quickSort(int values[], int start, int end);
+int partitionQuickSort(int values[], int start, int end);
+void insertionSort(int values[], int count);
+
+int partitionQuickSort(int values[], int start, int end) {
+	int pivot = 0;
+	int temp = 0, left = 0, right = 0;
+	left = start;
+	right = end;
+	pivot = end;
+
+	while (left < right) {
+		while ((values[left] < values[pivot]) && (left < right)) {
+			left++;
+		}
+		while ((values[right] >= values[pivot]) && (left < right)) {
+			right--;
+		}
+
+		if (left < right) {
+			temp = values[left];
+
+			values[left] = values[right];
+			values[right] = temp;
+			printf("start - [%d], end - [%d], pivot - [%d], in-loop,", start, end, values[pivot]);
+
+			printArray(values, end - start + 1);
+		}
+ 	}
+	temp = values[pivot];
+	values[pivot] = values[right];
+	values[right] = temp;
+
+	printf("start - [%d], end - [%d], pivot - [%d], out-loop,", start, end, values[right]);
+	printArray(values, end - start + 1);
+	
+	return right;
+}
+void main() {
+	int values[] = {80, 50, 70, 10, 60, 20, 40, 30};
+
+	printf("Before Sort\n");
+	printArray(values, 8);
+
+	//selectionSort(values, 8);
+	//BubbleSort(values, 8);
+	//quickSort(values, 0, 7);
+	insertionSort(values, 8);
+	printf("\nAfter Sort\n");
+	printArray(values, 8);
+
+
+}
+void insertionSort(int values[], int count) {
+	int i = 0, pre = 0, next = 0, j = 0;
+	int temp = 0;
+	for (int i = 1; i < count; i++) {
+		temp = values[i];
+		j = i;
+		while ((j > 0) && values[j - 1] > temp) {
+			values[j] = values[j - 1];
+			j--;
+		}
+		values[j] = temp;
+		printf("Step - %d,", i);
+		printArray(values, count);
+	}
+}
+
+void quickSort(int values[], int start, int end) {
+	int pivot = 0;
+	if (start < end) {
+		pivot = partitionQuickSort(values, start, end);
+		quickSort(values, start, pivot - 1);
+		quickSort(values, pivot + 1, end);
+	}
+}
+void BubbleSort(int values[], int count) {
+	int i = 0, j = 0, count1 = count, temp = 0;
+	for (i = 0; i < count; i++) {
+		for (j = 0; j < count1-1; j++) {
+			if (values[j] > values[j + 1]) {
+				temp = values[j + 1];
+				values[j + 1] = values[j];
+				values[j] = temp;
+			}
+		}
+		count1--;
+		printf("Step- %d ", i + 1);
+		printArray(values, count);
+	}
+}
+
+void selectionSort(int values[], int count) {
+	int temp = 0;
+	int i = 0, j = 0, min = 0, k = 0;
+
+	for (i = 0; i < count; i++) {
+		temp = values[i];
+		min = values[i];
+		for (j = i + 1; j < count; j++) {
+			if (temp > values[j]) {
+				temp = values[j];
+				min = j;
+			}
+		} 
+		values[min] = values[i];
+		values[i] = temp;
+
+		printf("Step- %d ", i + 1);
+		printArray(values, count);
+
+	}
+
+}
+void printArray(int values[], int count) {
+	for (int i = 0; i < count; i++) {
+		printf("%d ", values[i]);
+	}
+	printf("\n");
+}
